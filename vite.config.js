b5c7af932e815900fb8b8a7162abc5a67c4ab4b4
KineHFRZ <coordinacionkinebmqhrlbo@gmail.com>
2026-05-16 23:10:26 -0400

    fix: alias @ y remover plugin base44

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   logLevel: 'info',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [react()],
 })
